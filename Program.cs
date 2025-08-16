628cca060a6352da89bc622dcb78fd10911d7bc2
tatojastine-n <jajanicoletato@gmail.com>
2025-08-16 17:42:19 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Ingredient
+{
+    public string Name { get; set; }
+    public double Quantity { get; set; }
+    public string Unit { get; set; }
+
+    public Ingredient(string name, double quantity, string unit)
+    {
+        Name = name;
+        Quantity = quantity;
+        Unit = unit;
+    }
+
+    public Ingredient Scale(double scaleFactor)
+    {
+        return new Ingredient(Name, Quantity * scaleFactor, Unit);
+    }
+
+    public static double NormalizeUnit(double quantity, string unit)
+    {
+        switch (unit.ToLower())
+        {
+            case "kg":
+                return quantity * 1000; 
+            case "g":
+            default:
+                return quantity; 
+        }
+    }
+}
+
+public class Recipe
+{
+    public List<Ingredient> Ingredients { get; set; }
+    public double BaseServings { get; set; }
+
+    public Recipe(List<Ingredient> ingredients, double baseServings)
+    {
+        Ingredients = ingredients;
+        BaseServings = baseServings;
+    }
+
+    public List<Ingredient> ScaleRecipe(double targetServings)
+    {
+        double scaleFactor = targetServings / BaseServings;
+        return Ingredients.Select(ingredient => ingredient.Scale(scaleFactor)).ToList();
+    }
+
+    public List<(string Name, double Required, double Available)> CheckShortages(Dictionary<string, double> pantry)
+    {
+        var shortages = new List<(string Name, double Required, double Available)>();
+
+        foreach (var ingredient in Ingredients)
+        {
+            double requiredQuantity = Ingredient.NormalizeUnit(ingredient.Quantity, ingredient.Unit);
+            double availableQuantity = pantry.ContainsKey(ingredient.Name) ? pantry[ingredient.Name] : 0;
+
+            if (requiredQuantity > availableQuantity)
+            {
+                shortages.Add((ingredient.Name, requiredQuantity, availableQuantity));
+            }
+        }
+
+        return shortages;
+    }
+}
+
+namespace Recipe_Scaler___Pantry_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ingredients = new List<Ingredient>
+        {
+            new Ingredient("Flour", 500, "g"),
+            new Ingredient("Sugar", 200, "g"),
+            new Ingredient("Butter", 1, "kg")
+        };
+
+            var recipe = new Recipe(ingredients, 4); 
+
+            var scaledIngredients = recipe.ScaleRecipe(10);
+            Console.WriteLine("Scaled Ingredients:");
+            foreach (var ingredient in scaledIngredients)
+            {
+                Console.WriteLine($"{ingredient.Name}: {ingredient.Quantity} {ingredient.Unit}");
+            }
+
+            var pantry = new Dictionary<string, double>
+        {
+            { "Flour", 300 }, 
+            { "Sugar", 150 }, 
+            { "Butter", 0.5 } 
+        };
+
+            var shortages = recipe.CheckShortages(pantry);
+            Console.WriteLine("\nShopping List for Shortages:");
+            foreach (var shortage in shortages)
+            {
+                Console.WriteLine($"{shortage.Name}: Required {shortage.Required}g, Available {shortage.Available}g");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Recipe Scaler & Pantry Checker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Recipe Scaler & Pantry Checker")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("00efc7d8-60ab-4bd3-8aaa-4c4ed858cda1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
